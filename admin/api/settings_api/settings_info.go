bcd8d237d08000c3b0be9cc8ae4057f4e962586b
刘平凡 <liu2476979217@163.com>
2023-11-28 22:56:11 +0800

    封装响应

--- admin/api/settings_api/settings_info.go
+++ admin/api/settings_api/settings_info.go
@@ -1,7 +1,10 @@
 package settings_api
 
-import "github.com/gin-gonic/gin"
+import (
+	"admin/models/res"
+	"github.com/gin-gonic/gin"
+)
 
 func (SettingsApi) SettingsInfoView(c *gin.Context) {
-	c.JSON(200, gin.H{"msg": "xxx"})
+	res.FailWithCode(res.SettingError, c)
 }
