e08b5b469671643ddb443065d4da21176cf45e1a
刘平凡 <liu2476979217@163.com>
2023-11-29 22:42:31 +0800

    错误码封装

---
+++ admin/testdata/1.错误码读取.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"admin/models/res"
+	"encoding/json"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"os"
+)
+
+const file = "models/res/err_code.json"
+
+type ErrMap map[res.ErrorCode]string
+
+func main() {
+	byteData, err := os.ReadFile(file)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+	var errMap = ErrMap{}
+	err = json.Unmarshal(byteData, &errMap)
+	if err != nil {
+		logrus.Error(err)
+		return
+	}
+	fmt.Println(errMap)
+	fmt.Println(errMap[res.SettingError])
+}
